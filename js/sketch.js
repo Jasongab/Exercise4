44aacbcf270b79ab8f8602231f62e72be511b98c
Jasongab <jasongabaee@172-16-11-227.dynapool.nyu.edu>
2015-10-16 17:27:09 -0400

    Exercise 4

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,15 @@
 var imgbg //mask
 var imgBob;//spongebob
-
+var font;
 
 
 function preload() {
-imgbg=loadImage('img/images-3.jpg');// mask 
+imgbg=loadImage('img/squidward.jpg');// squidward 
 imgBob=loadImage('img/images-1.jpg');// spongebob
-imgPic=loadImage('img/images-2.jpg'); ///de la soul    
+imgPic=loadImage('img/bikini_bottom.jpg'); // bikni bottom background    
+
+font=loadFont('font/myfont.otf');
 }
-    
 
 
 
@@ -16,6 +17,8 @@ imgPic=loadImage('img/images-2.jpg'); ///de la soul
 function setup() {
 createCanvas(displayWidth,displayHeight);
 image(imgPic,0,0,displayWidth,displayHeight); 
+textSize(64);
+textFont(font);
 
    
 }
@@ -25,15 +28,20 @@ function draw(){
     image(imgBob, mouseX, mouseY);
     
     
-    var x1= random(0,displayWidth);
-    var y1=0;
+    var x1= random(0,displayWidth);//Gen Design P.294
+    var y1=0;//Gen Design P.294
     
-    var x2 = round(x1 + random(-7,7));
-    var y2 =round(random(-5,5));
+    var x2 = round(x1 + random(-7,7));//Gen Design P.294
+    var y2 =round(random(-5,5));//Gen Design P.294
     
-    var w = random(35,70)
-    var h = displayHeight; 
+    var w = random(35,70)//Gen Design P.294
+    var h = displayHeight; //Gen Design P.294
     
-    copy(x1,y1,w,h, x2,y2,w,h);
+    copy(x1,y1,w,h, x2,y2,w,h);//Gen Design P.294
+    
+    if (mouseX<200 && mouseY<100){
+       
+        text('Smell ya later SpongeBob!',100,50);
 
+}
 }
