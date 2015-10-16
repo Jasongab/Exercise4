428c864b9ce407e5e46278a6d4d8de574973903c
Jasongab <jasongabaee@172-16-11-227.dynapool.nyu.edu>
2015-10-16 16:42:26 -0400

    commit 2
    melting/meshing images!

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,12 @@
-var imgbg
-var imgBob;
+var imgbg //mask
+var imgBob;//spongebob
 
 
 
 function preload() {
-imgbg=loadImage('img/images-3.jpg');//
-imgBob=loadImage('img/spongeBob.png');// 
+imgbg=loadImage('img/images-3.jpg');// mask 
+imgBob=loadImage('img/images-1.jpg');// spongebob
+imgPic=loadImage('img/images-2.jpg'); ///de la soul    
 }
     
 
@@ -14,7 +15,25 @@ imgBob=loadImage('img/spongeBob.png');//
 
 function setup() {
 createCanvas(displayWidth,displayHeight);
+image(imgPic,0,0,displayWidth,displayHeight); 
 
-image(imgbg);
-image(imgBob);
+   
+}
+
+function draw(){
+    image(imgbg);
+    image(imgBob, mouseX, mouseY);
+    
+    
+    var x1= random(0,displayWidth);
+    var y1=0;
+    
+    var x2 = round(x1 + random(-7,7));
+    var y2 =round(random(-5,5));
+    
+    var w = random(35,70)
+    var h = displayHeight; 
+    
+    copy(x1,y1,w,h, x2,y2,w,h);
+
 }
